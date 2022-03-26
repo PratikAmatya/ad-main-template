a2d25ff4206fa99d3d80c2acf3c4a7700e5bba7a
PratikAmatya <pratikamatya2@gmail.com>
2022-03-26 16:24:32 +0545

    First Template

---
+++ crudApp/crudApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crudApp.Models;
+
+namespace crudApp.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
